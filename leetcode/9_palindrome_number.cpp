d0ebb3d830bce3211f12030dd7adda3779b6c6a8
june.kim <june.kim@kakaocorp.com>
2018-10-30 19:20:51 +0900

    leetcode 9. Palindrome Number

--- leetcode/9_palindrome_number.cpp
+++ leetcode/9_palindrome_number.cpp
@@ -7,9 +7,30 @@
 #include <iostream>
 #include <algorithm>
 
+using namespace std;
+
 class Solution {
 public:
     bool isPalindrome(int x) {
+        string s = to_string(x);
 
+        unsigned long half_size = s.size()/2;
+
+        string left = s.substr(0, half_size);
+        string right = s.substr(half_size + (s.size()%2 == 1 ? 1 : 0));
+        reverse(right.begin(), right.end());
+
+        if (left == right) {
+            return true;
+        }
+        return false;
     }
 };
+
+int main() {
+    Solution solution = Solution();
+    assert(solution.isPalindrome(121) == true);
+    assert(solution.isPalindrome(-121) == false);
+    assert(solution.isPalindrome(10) == false);
+    return 0;
+}
