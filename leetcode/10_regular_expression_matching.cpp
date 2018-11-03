7873463c9bfec3d8ba5b93111bed25c1f7a28058
june.kim <june.kim@kakaocorp.com>
2018-11-03 23:14:44 +0900

    base code of 10, 11

---
+++ leetcode/10_regular_expression_matching.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <cassert>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+// '.' Matches any single character.
+// '*' Matches zero or more of the preceding element.
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        return false;
+    }
+};
+
+int main() {
+    Solution solution = Solution();
+    assert(solution.isMatch("aa", "a") == false);
+    assert(solution.isMatch("aa", "a*") == true);
+    assert(solution.isMatch("ab", ".*") == true);
+    assert(solution.isMatch("aab", "c*a*b") == true);
+
+    assert(solution.isMatch("mississippi", "mis*is*p*.") == false);
+    assert(solution.isMatch("ab", ".*c") == false);
+    assert(solution.isMatch("aaa", "a*a") == true);
+    assert(solution.isMatch("aaa", "ab*a*c*a") == true);
+    return 0;
+}
---
+++ leetcode/11_container_with_most_water.cpp
@@ -0,0 +1,27 @@
+// 11. Container With Most Water
+// https://leetcode.com/problems/container-with-most-water/
+
+#include <string>
+#include <vector>
+#include <cassert>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+
+        return 0;
+    }
+};
+
+int main() {
+    Solution solution = Solution();
+    vector<int> nums({1,8,6,2,5,4,8,3,7});
+    assert(solution.maxArea(nums) == 49);
+
+    return 0;
+}
