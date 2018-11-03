498fe6353763ddf32a678c34f63e878881bf71e6
june.kim <june.kim@kakaocorp.com>
2018-11-03 23:19:18 +0900

    leetcode 11. Container With Most Water

--- leetcode/11_container_with_most_water.cpp
+++ leetcode/11_container_with_most_water.cpp
@@ -9,12 +9,25 @@
 
 using namespace std;
 
-
 class Solution {
 public:
     int maxArea(vector<int>& height) {
+        int max = 0;
 
-        return 0;
+        unsigned long length = height.size();
+        for (int i=0; i < length - 1; i++) {
+            int first_height = height[i];
+            for (int j=i+1; j<length; j++) {
+                int second_height = height[j];
+                int width = j-i;
+                int height = min(first_height, second_height);
+                int water = height * width;
+                if (water > max) {
+                    max = water;
+                }
+            }
+        }
+        return max;
     }
 };
 
