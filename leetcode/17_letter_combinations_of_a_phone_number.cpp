bfb70599e581f024fe4bb907b6f25cd984dc783e
june.kim <june.kim@kakaocorp.com>
2018-11-09 18:37:31 +0900

    leetcode 17. Letter Combinations of a Phone Number

--- leetcode/17_letter_combinations_of_a_phone_number.cpp
+++ leetcode/17_letter_combinations_of_a_phone_number.cpp
@@ -12,9 +12,55 @@
 using namespace std;
 
 class Solution {
+private:
+    map<char, string> number_map{
+        {'2', "abc"},
+        {'3', "def"},
+        {'4', "ghi"},
+        {'5', "jkl"},
+        {'6', "mno"},
+        {'7', "pqrs"},
+        {'8', "tuv"},
+        {'9', "wxyz"},
+    };
 public:
+    vector<string> mix(vector<string> left, vector<string> right) {
+        vector<string> result;
+
+        for (auto left_it : left) {
+            for (auto right_it : right) {
+                cout << left_it + right_it << endl;
+                result.push_back(left_it + right_it);
+            }
+        }
+
+        return result;
+    }
+
+    vector<string> string_to_vector(string str) {
+        vector<string> result;
+
+        for (auto c : str) {
+            string tmp;
+            tmp.push_back(c);
+            result.push_back(tmp);
+        }
+        return result;
+    }
+
     vector<string> letterCombinations(string digits) {
-        vector<string> result({"ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"});
+        vector<string> result;
+
+        unsigned long length = digits.size();
+
+        for (int i=0; i<length; i++) {
+            if (i == 0) {
+                result = string_to_vector(number_map[digits[i]]);
+            } else {
+                result = mix(result, string_to_vector(number_map[digits[i]]));
+            }
+        }
+
         return result;
     }
 
@@ -29,6 +75,12 @@ int main() {
     Solution solution = Solution();
 
     auto result = solution.letterCombinations("23");
+    solution.print(result);
+
+    result = solution.letterCombinations("2");
+    solution.print(result);
+
+    result = solution.letterCombinations("234");
     solution.print(result);
 
     return 0;
