5de1cead053a2ce50e7fb63929399c0ad805e17c
june.kim <june.kim@kakaocorp.com>
2018-11-10 12:54:22 +0900

    leetcode 18. 4Sum

--- leetcode/18_4sum.cpp
+++ leetcode/18_4sum.cpp
@@ -13,9 +13,37 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>> result;
+    vector<vector<int>> fourSum(vector<int> &nums, int target) {
+        set<vector<int>> temp_result;
+        sort(nums.begin(), nums.end());
 
+        unsigned long length = nums.size();
+        if (length < 4) {
+            vector<vector<int>> result;
+            return result;
+        }
+
+        for (int i = 0; i < length - 3; i++) {
+            if (i + 4 < length and nums[i] == nums[i + 4]) {
+                continue;
+            }
+            for (int j = i + 1; j < length - 2; j++) {
+                int pre_sum = nums[i] + nums[j];
+
+                for (int k = j + 1; k < length - 1; k++) {
+                    int pre_sum2 = pre_sum + nums[k];
+                    int find = target - pre_sum2;
+
+                    auto low = lower_bound(nums.begin() + k + 1, nums.end(), find);
+
+                    if (low != nums.end() && !(find < *low)) {
+                        vector<int> row({nums[i], nums[j], nums[k], nums[low - nums.begin()]});
+                        temp_result.insert(row);
+                    }
+                }
+            }
+        }
+        vector<vector<int>> result(temp_result.begin(), temp_result.end());
         return result;
     }
 
@@ -39,6 +67,16 @@ int main() {
     auto result = solution.fourSum(input1, 0);
     solution.printVector(result);
     assert(result.size() == 3);
+
+    vector<int> input2({0, 0, 0, 0});
+    result = solution.fourSum(input2, 0);
+    solution.printVector(result);
+    assert(result.size() == 1);
+
+    vector<int> input3({0, 0, 0, 0});
+    result = solution.fourSum(input3, 1);
+    solution.printVector(result);
+    assert(result.size() == 0);
 
     return 0;
 }
