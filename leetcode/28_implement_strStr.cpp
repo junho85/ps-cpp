682d6e84ac81d6789bc93568e8e96ec2d266bb65
june.kim <june.kim@kakaocorp.com>
2018-11-17 21:23:49 +0900

    leetcode 28. Implement strStr()

--- leetcode/28_implement_strStr.cpp
+++ leetcode/28_implement_strStr.cpp
@@ -1,6 +1,29 @@
+// 28. Implement strStr()
+// https://leetcode.com/problems/implement-strstr/
+
+#include <string>
+#include <vector>
+#include <cassert>
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <stack>
+
+using namespace std;
+
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-
+        return haystack.find(needle, 0);
     }
 };
+
+int main() {
+    Solution solution = Solution();
+
+    assert(solution.strStr("hello", "ll") == 2);
+    assert(solution.strStr("aaaaa", "bba") == -1);
+
+    return 0;
+}
