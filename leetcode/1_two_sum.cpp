fe648bd2884999f78e1e786ebdfbb7ac25d25e5d
june.kim <june.kim@kakaocorp.com>
2018-12-01 23:35:48 +0900

    leetcode 1. Two Sum - use map

--- leetcode/1_two_sum.cpp
+++ leetcode/1_two_sum.cpp
@@ -1,11 +1,36 @@
 #include <vector>
 #include <iostream>
+#include <map>
 
 using namespace std;
 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> answer(2);
+
+        map<int, int> map_nums; // number, index
+
+        // put into map
+        auto length = nums.size();
+        for (int i=0; i<length; i++) {
+            map_nums[nums[i]] = i;
+        }
+
+        for (int i = 0; i < length; i++) {
+            int find_num = target - nums[i];
+            auto iter = map_nums.find(find_num);
+
+            if (iter != map_nums.end() && iter->second != i) {
+                answer[0] = i;
+                answer[1] = iter->second;
+                return answer;
+            }
+        }
+        return answer;
+    }
+
+    vector<int> twoSum_old(vector<int>& nums, int target) {
         vector<int> answer(2);
 
         int right = nums[1];
@@ -22,16 +47,23 @@ public:
                 }
             }
         }
+        return answer;
     }
 };
 
+
 int main() {
-//    vector<int> nums({2,7,11,15});
-    vector<int> nums({3,2,4});
+    vector<int> nums({2,7,11,15});
 
     Solution solution = Solution();
-//    vector<int> answer = solution.twoSum(nums, 9);
-    vector<int> answer = solution.twoSum(nums, 6);
+    vector<int> answer = solution.twoSum(nums, 9);
+    cout << answer[0] << ":" << answer[1] << endl;
+
+    nums.clear();
+    nums.push_back(3);
+    nums.push_back(2);
+    nums.push_back(4);
+    answer = solution.twoSum(nums, 6);
 
     cout << answer[0] << ":" << answer[1] << endl;
     return 0;
