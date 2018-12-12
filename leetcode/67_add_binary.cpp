90ee663357a30f1ad38af7e018e1d42e93baf907
june.kim <june.kim@kakaocorp.com>
2018-12-12 22:16:45 +0900

    leetcode 67. Add Binary. modified to another way. but it's not work on leetcode. compiler problem?

--- leetcode/67_add_binary.cpp
+++ leetcode/67_add_binary.cpp
@@ -16,18 +16,43 @@ using namespace std;
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int max_length = max(a.size(), b.size());
-        long long sum = stoll(a, nullptr, 2) + stoll(b, nullptr, 2);
+        unsigned long max_length = max(a.size(), b.size());
 
         string result;
-        while (sum != 0) {
-            if (sum % 2 == 0) {
-                result = "0" + result;
-            } else {
-                result = "1" + result;
+
+        bool is_up = false;
+
+        for (int i = 0; i < max_length; i++) {
+            int current = 0;
+            if (is_up) {
+                current++;
+                is_up = false;
             }
-            sum /= 2;
+
+            auto a_last = a.back();
+            if (a_last == '1') current++;
+            a.pop_back();
+
+            auto b_last = b.back();
+            if (b_last == '1') current++;
+            b.pop_back();
+
+            if (current >= 2) {
+                is_up = true;
+                current %= 2;
+            }
+
+            if (current == 0) {
+                result = '0' + result;
+            } else {
+                result = '1' + result;
+            }
         }
+
+        if (is_up) {
+            result = '1' + result;
+        }
+
         return result;
     }
 };
@@ -39,7 +64,8 @@ int main() {
     assert(solution.addBinary("11", "1") == "100");
     assert(solution.addBinary("1010", "1011") == "10101");
 
-    assert(solution.addBinary("10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101", "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011") == "");
+    assert(solution.addBinary("10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101", "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011") == "110111101100010011000101110110100000011101000101011001000011011000001100011110011010010011000000000");
+    assert(solution.addBinary("1", "111") == "1000");
 
     return 0;
 }
