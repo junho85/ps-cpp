db5697ab8989b5fe025b7c6bb0ac95136c64d538
june.kim <junho85@gmail.com>
2022-08-05 18:36:30 +0900

    정리

--- 01.algorithm/minmax.cpp
+++ 01.algorithm/minmax.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 int main() {
-    cout << 9223372036854775807 + 1 << endl;
-    // -9223372036854775808
-
     cout << min(1, 2) << endl;
     cout << max(1, 2) << endl;
 
